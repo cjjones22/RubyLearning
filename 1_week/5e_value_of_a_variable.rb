c3b90f1c8ec060d2893e56b59968f4a3c66154c8
cjjones22 <cjjones22@gmail.com>
2014-01-15 18:39:18 -0600

    Doctest notes for method calling in various ways
    This clarifies and shows how to remove ambiguous method calls.
    
    By that I mean, how to call a method that has the same name of a
    variable in the same scope.

--- 1_week/5e_value_of_a_variable.rb
+++ 1_week/5e_value_of_a_variable.rb
@@ -1,5 +1,18 @@
+# doctest: variables will be called when it is ambiguous
+# >> my_string
+# => "Hello Ruby World"
+# doctest: send(:my_string)
+# >> "Hello World"
+# doctest: my_string()
+# >> my_string()
+# >> "Hello World"
+# doctest: self.my_string
+# You can not use a receiver for private methods
+# >> self.my_string
+# >> "Hello World"
 my_string = 'Hello Ruby World' 
-
+# If I define methods at the 'main' level they are by default private.
+public
 def my_string
   'Hello World' 
 end
